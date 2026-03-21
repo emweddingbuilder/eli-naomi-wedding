6cfb3a209c4a66e8a172840322af13678c0dd05a
emweddingbuilder <eminsky1@gmail.com>
2026-03-21 20:43:45 +0200

    hero: adjust photo position to show more of upper body

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -295,7 +295,7 @@ export default function LandingPage() {
               position: 'absolute', inset: 0,
               backgroundImage: "url('/hero.jpg')",
               backgroundSize: 'cover',
-              backgroundPosition: 'center center',
+              backgroundPosition: 'center 20%',
               backgroundRepeat: 'no-repeat',
               filter: 'brightness(0.75)',
             }}
